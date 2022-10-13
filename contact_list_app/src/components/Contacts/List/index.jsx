47626d80ca4cde41e80ef0ad1716a27d8eec5d43
unaldutlu <unaldutlu@gmail.com>
2022-10-13 20:19:11 +0300

    filter added

--- contact_list_app/src/components/Contacts/List/index.jsx
+++ contact_list_app/src/components/Contacts/List/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
-function List() {
-  return <div>Contact List</div>;
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const onChangeText = (e) => {
+    setFilterText(e.target.value);
+  };
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <input
+          placeholder='Filter contact'
+          value={filterText}
+          onChange={onChangeText}
+        />
+      </div>
+      <ul>
+        {filtered.map((contact, e) => (
+          <li key={e}>
+            <span>{contact.fullname}</span>
+            <span>{contact.phone_number}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default List;
--- contact_list_app/src/components/Contacts/index.jsx
+++ contact_list_app/src/components/Contacts/index.jsx
@@ -3,15 +3,18 @@ import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    { fullname: "Unal", phone_number: 12345 },
+    { fullname: "Mehmet", phone_number: 98765 },
+  ]);
 
-  useEffect(()=>{
-    console.log(contacts);
-  },[contacts])
+  // useEffect(()=>{
+  //   console.log(contacts);
+  // },[contacts])
 
   return (
-    <div>
-      <List />
+    <div className="">
+      <List contacts={contacts} />
       <Form addContacts={setContacts} contacts={contacts} />
     </div>
   );
